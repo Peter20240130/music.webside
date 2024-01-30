149102573e634fa9c3971e51d78b2243fd759cbf
Peter20240130 <158176938+Peter20240130@users.noreply.github.com>
2024-01-30 22:29:08 +0800

    Add files via upload

---
+++ 音樂/login.js
@@ -0,0 +1,11 @@
+var password = 20110721;
+var input;
+while(password!=input){
+    input = prompt('請輸入密碼');
+    if(input != password){
+        alert('密碼錯誤，請重新輸入！');
+    }
+}
+alert('登入成功');
+alert('歡迎來到會員推薦音樂網站');
+alert('這裡會有幾首我推薦的音樂，但都純個人看法');
